8a356a3ac109a120647e4df51c59e85a9bf27471
mallikarjun-edara <63585043+mallikarjun-edara@users.noreply.github.com>
2021-02-06 17:57:30 -0600

    Update 1_icp2.py

--- source code/1_icp2.py
+++ source code/1_icp2.py
@@ -1,1 +1,13 @@
-
+#1. Write a program, which reads height (feet.) of N students into a list and convert these heights to cm in a separate list:
+L1=[]### list array
+n=int(input("Enter number of students :"))### asking for input from user
+for i in range(0,n):
+    ele = input()
+    L1.append(ele)### appending inputs from user
+print(L1)##printng list
+abc=float(L1[0])
+de=float(L1[1])
+ghi=float(L1[2])
+jkl=float(L1[3])
+cm=[abc/0.0328,de/0.0328,ghi/0.0328,jkl/0.0328]##validating user input to output
+print("length in cm:",round(cm[0],1),round(cm[1],1),round(cm[2],1),round(cm[3],1))### printing output
